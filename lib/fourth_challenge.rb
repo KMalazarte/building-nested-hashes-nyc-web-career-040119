f9217486cd58d8f892c757c094224057048706c7
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-18 00:14:50 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [ 
         {name: "Benvolio", age: "17", attitude: "worried"}
-        :second_friend => {name: "Mercutio", age: "18", attitue: "hot-headed"}
+        {name: "Mercutio", age: "18", attitue: "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -14,11 +14,9 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+       {name: "Steven", age: "30", attitude: "confused"}
+       {name: "'Nurse'", age: "44", attitude: "worried"}
       ]
    }
 }
-
-  
-
 end
